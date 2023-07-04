d357735ca6f40a20f44c0ed89216ce5ee74ad120
Bruno Cordeiro <brunocordeiroriberi130904@gmail.com>
2023-07-04 10:10:05 -0300

    Update

---
+++ chessmain.py
@@ -0,0 +1,13 @@
+import pygame as p
+from Chess import ChessEngine
+
+ALTURA = LARGURA = 512
+DIMENSAO = 8
+QUADRADO_TAMANHO = ALTURA // LARGURA
+MAX_FPS = 15
+IMAGENS = {}
+
+def CarregarImagens():
+    IMAGENS[]
+ 
+
